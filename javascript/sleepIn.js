f27656cb7f3be89bcb7abf728c00872a04022720
Robert DeRieux <ppldiecomplex@gmail.com>
2024-12-02 11:33:07 -0700

    Add jest testing, add sleepIn function

---
+++ javascript/sleepIn.js
@@ -0,0 +1,14 @@
+/*
+The parameter weekday is true if it is a weekday, 
+and the parameter vacation is true if we are on vacation. We sleep 
+in if it is not a weekday or we're on vacation. Return true if we sleep in.
+*/
+function sleepIn(weekday, vacation) {
+  // if weekday is not true or vacation is true we sleep in,
+  // else we do not sleep in
+  if (!weekday || vacation) {
+    return true;
+  } else {
+    return false;
+  }
+}
