5046c7e299872efa980f2c11d44b11084705fcf8
Robert DeRieux <ppldiecomplex@gmail.com>
2024-12-09 15:29:48 -0700

    add frontTimes function

--- src/warmup2/frontTimes.js
+++ src/warmup2/frontTimes.js
@@ -1,3 +1,11 @@
+/**
+ *
+ * @param {string} str
+ * @param {number} n
+ * @returns a str of the first 3 chars of provided str,
+ * repeating n number of times.  Whatever is there repeating n times,
+ * if less then 3 chars.
+ */
 export default function frontTimes(str, n) {
   // If the str is less than 3, loop through n times
   // adding the str to a new str variable
