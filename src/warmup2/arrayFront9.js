5436fe88dcebb0c4522ca574e0924575c8cb1d11
Robert DeRieux <ppldiecomplex@gmail.com>
2024-12-10 16:40:16 -0700

    Add arrayFront9 function

--- src/warmup2/arrayFront9.js
+++ src/warmup2/arrayFront9.js
@@ -10,14 +10,16 @@ export default function arrayFront9(numberArray) {
   // if current index of array equals 9 add 1 to count
   // return true if count is greater than 0
   let count = 0;
+  let end = numberArray.length < 5 ? numberArray.length : 4;
   if (!Array.isArray(numberArray)) {
     return "error";
   }
-  for (let i = 0; i < numberArray.length || i < numberArray[5]; i++) {
+  for (let i = 0; i < end; i++) {
     if (numberArray[i] === 9) {
       count += 1;
     }
   }
+
   if (count > 0) {
     return true;
   } else {
