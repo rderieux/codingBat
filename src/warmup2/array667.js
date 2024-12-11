a6459ac3c74dbeab664a93aa5b8fec819928e94c
Robert DeRieux <ppldiecomplex@gmail.com>
2024-12-11 15:20:09 -0700

    Add array667 function, and pseudo code

---
+++ src/warmup2/array667.js
@@ -0,0 +1,7 @@
+export default function array667(nums) {
+  // Declare a count varialbe
+  // loop through the given array
+  // if current index is 6 and index + 1 is 6
+  // OR current index is 6 and index + 1 is 7
+  // add 1 to count, return count
+}
