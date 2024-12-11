3a908d0c0a8a681a7dd6a1ae7844dd06d3853705
Robert DeRieux <ppldiecomplex@gmail.com>
2024-12-10 17:52:21 -0700

    Add stringMatch function & JSdocs

--- src/warmup2/stringMatch.js
+++ src/warmup2/stringMatch.js
@@ -1,1 +1,22 @@
-export default function stringMatch(a, b) {}
+export default function stringMatch(a, b) {
+  // Declare a count varialbe
+  // find out which string is shorter
+  // loop through shorter string
+  // grab the substring at index and next char for both
+  // compare the substrings, if equal add to count
+  // return count
+  if (typeof a !== "string" || typeof b !== "string") {
+    return "error";
+  }
+  let count = 0;
+  const length = Math.min(a.length, b.length);
+  for (let i = 0; i < length - 1; i++) {
+    const aSubstring = a.substring(i, i + 2);
+    const bSubstring = b.substring(i, i + 2);
+
+    if (aSubstring === bSubstring) {
+      count += 1;
+    }
+  }
+  return count;
+}
