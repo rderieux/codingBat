a44799542cba8944b286c7af40297ad05b94bf12
Robert DeRieux <ppldiecomplex@gmail.com>
2024-12-12 14:39:18 -0700

    Add tests for a function to - when given two strings a & b - return string abba

--- test/string1.test.js
+++ test/string1.test.js
@@ -3,7 +3,6 @@ import { expect } from "chai";
 import helloName from "../src/string1/helloName.js";
 
 //helloName
-//helloName
 describe("#helloName()", () => {
   describe("works with correct types", () => {
     it("is a function", () => {
@@ -30,6 +29,38 @@ describe("#helloName()", () => {
         expect(helloName("Alpha")).to.equal("Hello Alpha!");
         expect(helloName("Omega")).to.equal("Hello Omega!");
         expect(helloName("God")).to.equal("Hello God!");
+      });
+    });
+  });
+});
+
+//makeAbba
+describe("#makeAbba()", () => {
+  describe("works with correct types", () => {
+    it("is a function", () => {
+      expect(makeAbba).to.be.a("function");
+    });
+    it("returns a string", () => {
+      expect(makeAbba("Bob", "by")).to.be.a("string");
+    });
+    describe("returns the correct output", () => {
+      it("returns 'error' if the argument is not a string", () => {
+        expect(makeAbba([])).to.equal("error");
+        expect(makeAbba({})).to.equal("error");
+        expect(makeAbba(1)).to.equal("error");
+        expect(makeAbba(undefined)).to.equal("error");
+        expect(makeAbba(null)).to.equal("error");
+      });
+    });
+
+    describe("returns the correct string", () => {
+      it("returns a greeting of the form 'Hello name!'.", () => {
+        expect(makeAbba("Bob", "by")).to.equal("BobbybyBob");
+        expect(makeAbba("Hi", "Bye")).to.equal("HiByeByeHi");
+        expect(makeAbba("", "")).to.equal("");
+        expect(makeAbba("Wad", "dup")).to.equal("WaddupdupWad");
+        expect(makeAbba("a", "b")).to.equal("abba");
+        expect(makeAbba("Ya", "Ya")).to.equal("YaYaYaYa");
       });
     });
   });
