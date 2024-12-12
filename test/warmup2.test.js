f138186cf89fff55bd9e6479292d8d6be39bda2f
Robert DeRieux <ppldiecomplex@gmail.com>
2024-12-12 12:36:55 -0700

    Add tests for a function to return a greeting in the form of 'Hello name!'

--- test/warmup2.test.js
+++ test/warmup2.test.js
@@ -363,6 +363,39 @@ describe("#has271()", () => {
         expect(has271([1, 2, 8, 1])).to.equal(false);
         expect(has271([3, 8, 2])).to.equal(true);
         expect(has271([2, 7, 3])).to.equal(true);
+        expect(has271([2, 7, 4])).to.equal(false);
+      });
+    });
+  });
+});
+
+//helloName
+describe("#helloName()", () => {
+  describe("works with correct types", () => {
+    it("is a function", () => {
+      expect(helloName).to.be.a("function");
+    });
+    it("returns a string", () => {
+      expect(helloName("Bob")).to.be.a("string");
+    });
+    describe("returns the correct output", () => {
+      it("returns 'error' if the argument is not a string", () => {
+        expect(helloName([])).to.equal("error");
+        expect(helloName(1)).to.equal("error");
+        expect(helloName({})).to.equal("error");
+        expect(helloName(undefined)).to.equal("error");
+        expect(helloName(null)).to.equal("error");
+      });
+    });
+
+    describe("returns the correct string", () => {
+      it("returns a greeting of the form 'Hello name!'.", () => {
+        expect(helloName("Bob")).to.equal("Hello Bob!");
+        expect(helloName("Dude")).to.equal("Hello Dude!");
+        expect(helloName("Bro")).to.equal("Hello Bro!");
+        expect(helloName("Alpha")).to.equal("Hello Alpha!");
+        expect(helloName("Omega")).to.equal("Hello Omega!");
+        expect(helloName("God")).to.equal("Hello God!");
       });
     });
   });
