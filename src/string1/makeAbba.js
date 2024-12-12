dbf173791b7ecdb065e1d7d08dacbc4d72cf1d68
Robert DeRieux <ppldiecomplex@gmail.com>
2024-12-12 14:44:20 -0700

    Add JSdocs for makeAbba

--- src/string1/makeAbba.js
+++ src/string1/makeAbba.js
@@ -1,3 +1,9 @@
+/**
+ *
+ * @param {string} a
+ * @param {string} b
+ * @returns a string of a + b + b + a.
+ */
 export default function makeAbba(a, b) {
   if (typeof a !== "string" || typeof b !== "string") {
     return "error";
