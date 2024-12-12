66ff377f85c941e6e9bad80b27139339041f6389
Robert DeRieux <ppldiecomplex@gmail.com>
2024-12-12 14:33:29 -0700

    Add JSdocs for helloName function

--- src/string1/helloName.js
+++ src/string1/helloName.js
@@ -1,3 +1,8 @@
+/**
+ *
+ * @param {string} name
+ * @returns a string in the form of a greeting "Hello name!".
+ */
 export default function helloName(name) {
   if (typeof name !== "string") {
     return "error";
