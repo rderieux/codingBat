e59cf5bde66fef2ea0740f96c06cff297cac249b
Robert DeRieux <ppldiecomplex@gmail.com>
2024-12-16 16:16:11 -0700

    Add tests for a function that, given a string, returns a string where every char is doubled

--- test/string1.test.js
+++ test/string1.test.js
@@ -1114,3 +1114,36 @@ describe("#withoutX2()", () => {
     });
   });
 });
+
+//doubleChar
+describe("#doubleChar()", () => {
+  describe("works with correct types", () => {
+    it("is a function", () => {
+      expect(doubleChar).to.be.a("function");
+    });
+    it("returns a string", () => {
+      expect(doubleChar("abcd")).to.be.a("string");
+    });
+    describe("receives the correct input", () => {
+      it("returns 'error' if the argument is not a string", () => {
+        expect(doubleChar([])).to.equal("error");
+        expect(doubleChar({})).to.equal("error");
+        expect(doubleChar(1)).to.equal("error");
+        expect(doubleChar(undefined)).to.equal("error");
+        expect(doubleChar(null)).to.equal("error");
+      });
+    });
+
+    describe("returns the correct string", () => {
+      it("given a string, return a string where every char in the original, there are two chars", () => {
+        expect(doubleChar("The")).to.equal("TThhee");
+        expect(doubleChar("abc")).to.equal("aabbcc");
+        expect(doubleChar("Hello")).to.equal("HHeelloo");
+        expect(doubleChar("xy")).to.equal("xxyy");
+        expect(doubleChar("i")).to.equal("ii");
+        expect(doubleChar("")).to.equal("");
+        expect(doubleChar("xxxx")).to.equal("xxxxxxxx");
+      });
+    });
+  });
+});
